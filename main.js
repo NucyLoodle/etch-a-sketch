6acb6120ba7046f7cc83d5e8837801bc30f21d49
Loodle <lucymeredith3@gmail.com>
2022-09-03 08:36:54 +0100

    Fix js loading before HTML

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-function boxes(){
-    let container = document.getElementById('body');
-    let x = Math.floor(window.innerWidth/4); //width of boxes
-    let y = Math.floor(window.innerHeight/4); //height of boxes
-    for (let i = 0; i < x * y; i++) {//multiply x*y to get total area of boxes that can fit
-        let box = document.createElement('div');//create a div
-        box.className = 'box';//assign class
-        container.appendChild(box);//append
-
-}};
-
-window.addEventListener('load', function() {
-    boxes();
-}); //loading webpage executes boxes function
+window.onload = () => {
+    function boxes(){
+        let container = document.getElementById('body');
+        let x = Math.floor(window.innerWidth/4); //width of boxes
+        let y = Math.floor(window.innerHeight/4); //height of boxes
+        for (let i = 0; i < x * y; i++) {//multiply x*y to get total area of boxes that can fit
+            let box = document.createElement('div');//create a div
+            box.className = 'box';//assign class
+            container.appendChild(box);//append
+    
+    }};
+    
+    window.addEventListener('load', function() {
+        boxes();
+    }); //loading webpage executes boxes function
+}
