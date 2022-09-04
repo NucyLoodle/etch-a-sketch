7328cd03f62ce19160983d5549e93f7714a8686b
Loodle <lucymeredith3@gmail.com>
2022-09-04 20:12:48 +0100

    Fix mouseover to change colour

--- main.js
+++ main.js
@@ -3,21 +3,14 @@ const container = document.querySelector('#container');
 for (i = 1; i <257; i++){
     const box = document.createElement('box');
     box.classList.add('box');
+    
     container.appendChild(box); 
-
-    box.addEventListener("mouseover", addColor);
+    box.addEventListener("mouseover",function handleMouseOver(){
+    box.style.backgroundColor = "#C2EDCE";
+    }); //Change colour of boxes on mouse hover
+   
+    
 } //Creates 16 boxes
 
-function addColor(e) {
-    e.target.style = `background-color: red`;
-} // Adds colour to boxes
-
-//To do - it's only happening on a click!
-
-//const box = document.querySelector('box');
-//const box = document.querySelector('box');
-//box.addEventListener("mouseover",function handleMouseOver(){
-    //box.style.backgroundColor = "#00FF00";
-//}); //Change colour of boxes on mouse hover
 
 
