e3efb38ee30c0024b5d42698bba6ef9becb5577a
Loodle <lucymeredith3@gmail.com>
2022-09-04 15:48:03 +0100

    Add colour to boxes

--- main.js
+++ main.js
@@ -4,12 +4,20 @@ for (i = 1; i <257; i++){
     const box = document.createElement('box');
     box.classList.add('box');
     container.appendChild(box); 
+
+    box.addEventListener("mouseover", addColor);
 } //Creates 16 boxes
 
-const box = document.querySelector('box');
-box.addEventListener("mouseover",function handleMouseOver(){
-    box.style.backgroundColor = "#00FF00";
-}); //Change colour of boxes on mouse hover
+function addColor(e) {
+    e.target.style = `background-color: red`;
+} // Adds colour to boxes
 
-//To do - it's taking a long time to change colour!
+//To do - it's only happening on a click!
+
+//const box = document.querySelector('box');
+//const box = document.querySelector('box');
+//box.addEventListener("mouseover",function handleMouseOver(){
+    //box.style.backgroundColor = "#00FF00";
+//}); //Change colour of boxes on mouse hover
+
 
