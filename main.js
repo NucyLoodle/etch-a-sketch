c413cb8c8567b22bb866d91956fd60bdc8dd80b1
Loodle <lucymeredith3@gmail.com>
2022-09-07 06:01:12 +0100

    Rewrite container generator as function

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-const container = document.querySelector('#container');
+createContainer();
+getSize();
 
-let userInput = prompt("Please enter the side length of the square."); //Asks user to input a value
-let num = parseInt(userInput);
 
-for (i = 1; i <num*num; i++){
+
+function createContainer(size) { //take any number and arrange as a grid
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${size},1fr`;
+    container.style.gridTemplateRows = `repeat(${size},1fr`;
+    let numBoxes = size * size;
+
+for (i = 1; i <numBoxes; i++){
     const box = document.createElement('box');
-    box.classList.add('box');
+    container.insertAdjacentElement("beforeend",box)
     
-    container.appendChild(box); 
+    //container.appendChild(box); 
     box.addEventListener("mouseover",function handleMouseOver(){
     box.style.backgroundColor = "#C2EDCE";
     }); //Change colour of boxes on mouse hover
-   
-    
 } //Creates boxes
 
+}
 
-
+function getSize() {
+    let userInput = parseInt.apply(prompt("Please enter the side length of the square.")); //Asks user to input a value
+    
+}
