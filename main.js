2565c885f4e930d06b34e3428bac1c00f4e1bc0e
Loodle <lucymeredith3@gmail.com>
2022-09-07 19:36:36 +0100

    Write user input as a function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-createContainer();
-getSize();
 
 
+function getSize() {
+    let userInput = parseInt.apply(prompt("Please enter the side length of the square.")); //Asks user to input a value
+}
 
 function createContainer(size) { //take any number and arrange as a grid
+    
     const container = document.querySelector('#container');
     container.style.gridTemplateColumns = `repeat(${size},1fr`;
     container.style.gridTemplateRows = `repeat(${size},1fr`;
@@ -18,10 +20,9 @@ for (i = 1; i <numBoxes; i++){
     box.style.backgroundColor = "#C2EDCE";
     }); //Change colour of boxes on mouse hover
 } //Creates boxes
-
 }
 
-function getSize() {
-    let userInput = parseInt.apply(prompt("Please enter the side length of the square.")); //Asks user to input a value
-    
-}
+
+
+
+
