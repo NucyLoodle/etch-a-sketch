d46a7025b2bacc8ebce204dcbf2e23711b920741
Loodle <lucymeredith3@gmail.com>
2022-09-11 07:22:43 +0100

    Remove attributes on boxes to fix loading

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-
-
 createContainer(16); //Default size
 
 let btn_popup = document.querySelector("#btnQuestion");
 btn_popup.addEventListener("click", function() {
-
-    document.getElementById("box").onclick = function() {
-        let node = document.getElementById("container");
-        node.parentNode.removeChild(node);
-    }
-   
-    //let removeBoxes = document.querySelectorAll('box');
-    //removeBoxes.container.removeChild(box); //Trying to remove boxes on load
     let size = getSize();
     createContainer(size);
 }) //User can click button to enter a number
-
-
-
 
 
 function getSize() {
@@ -34,12 +21,9 @@ function createContainer(size) { //take any number and arrange as a grid
     container.style.gridTemplateRows = `repeat(${size},1fr`;
     let numBoxes = size * size;
 
-for (let i = 1; i <=numBoxes; i++){
-    const box = document.createElement('div');//Element is a div
-   
-    
+for (i = 1; i <=numBoxes; i++){
+    const box = document.createElement('box');
     container.insertAdjacentElement("beforeend", box)
-    box.setAttribute('id','box'); //Setting id
     
     //container.appendChild(box); 
     box.addEventListener("mouseover",function handleMouseOver(){
@@ -48,6 +32,6 @@ for (let i = 1; i <=numBoxes; i++){
 } //Creates boxes
 }
 
-//To do: remove boxes on load
+
 
 
